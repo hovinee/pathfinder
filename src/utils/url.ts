39b629ca37e8c84d12dd53a72963a8f5d39ace45
hobinyoo <97422131+hobinyoo@users.noreply.github.com>
2024-01-22 14:28:31 +0900

    url 변경

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,6 @@
 export const getBaseUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : `${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : `https://pathfinder-bice-eta.vercel.app`
 
 export const cfWorkerUrl = 'https://futureverse-worker.team3.workers.dev'
