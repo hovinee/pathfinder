3306bacd9914b70d57872a5d5857a712a41820df
hobinyoo <97422131+hobinyoo@users.noreply.github.com>
2024-01-24 15:08:28 +0900

    배포환경 테스트

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.imweb.me', 'i.ytimg.com', 'cphinf.pstatic.net'],
+    domains: [
+      'cdn.imweb.me',
+      'i.ytimg.com',
+      'cphinf.pstatic.net',
+      'imagedelivery.net',
+    ],
   },
 }
 
